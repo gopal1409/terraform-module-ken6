da9662beb7a59e742c8232bb539de1e2539c27af
gopal <gopal1409@gmail.com>
2024-06-14 13:23:24 +0530

    data block

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,16 +1,11 @@
 #how to create a resource group. 
 #how to create a resource group. 
-resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
-  name = "${local.resource_name_prefix}-${var.resource_group_name}"
-  #sap-dev-rg-default
-  location = var.resource_group_location
-  tags     = local.common_tags
-}
+
 resource "azurerm_virtual_network" "vnet" {
   name = "${local.resource_name_prefix}-${var.vnet_name}"
   #this vnet need to be part of your resource group and resource group location
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = var.resource_group_name
+  resource_group_name = var.resource_group_location
   address_space       = var.vnet_address_space
 
   tags = local.common_tags
@@ -18,8 +13,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 resource "azurerm_subnet" "web_subnet" {
   name                 = "${local.resource_name_prefix}-${var.web_subnet_name}"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.resource_group_location
   address_prefixes     = var.web_subnet_address
 }
 
